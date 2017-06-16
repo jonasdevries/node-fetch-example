0e5651d590806f3fcf0f89856dba9d04b68e4354
Jonas Devries <jonas@good-it.be>
2017-06-16 15:34:56 +0200

    rm logging

--- request-promise.js
+++ request-promise.js
@@ -1,13 +1,11 @@
 let rp;
 if(process.env.USERNAME === 'JDVRIES') {
-  rp = require('request-promise').defaults({proxy:'http://vdabprdproxy.vdab.be:8080/', agent:false});
-  console.log('with vdab proxys')
+  rp = require('request-promise').defaults({proxy:'http://vdabprdproxy.vdab.be:8080/', agent:false});  
 } else {
    rp = require('request-promise');
-   console.log('without vdab proxy')
 }
 
 rp('https://jsonplaceholder.typicode.com/posts/1')
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
 
